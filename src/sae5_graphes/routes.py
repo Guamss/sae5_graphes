d92eaf9ccbbe6b6edad71ddba246eef520df825b
Florian <courrierdsf@gmail.com>
2025-01-09 01:09:26 +0100

    [API] - PUT pour metre à jour les poids de la grille

--- src/sae5_graphes/routes.py
+++ src/sae5_graphes/routes.py
@@ -12,12 +12,16 @@ api = SpecTree('flask')
 grille = models.Grille(20, 20)
 
 
-class GrilleDimensions(BaseModel):
+class GridSize(BaseModel):
     height: int = Field(20, title="Hauteur de la grille", description="Hauteur de la grille entre 2 et 1000")
     width: int = Field(20, title="Largeur de la grille", description="Largeur de la grille entre 2 et 1000")
 
 
-@app.route('/getGridDimensions', methods=['GET'])
+class GridWeights(BaseModel):
+    grid: list[list[int]] = Field([[1, 3, 5], [3, 1, 5]], title="Tableau 2D de la grille", description="Liste des poids de la grille")
+
+# ---- Actions sur la grille ---- #
+@app.route('/grid/dimensions', methods=['GET'])
 @api.validate(tags=["Grille"])
 def get_grid_dimensions():
     """
@@ -31,15 +35,16 @@ def get_grid_dimensions():
         if response:
             return jsonify(response), 200
         else:
-            return jsonify({"error": "Les dimensions de la grille n'ont pas pu être déterminés"}), 404
+            return jsonify({"error": "Les dimensions de la grille n'ont pas pu être déterminés"}), 400
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/editGrid', methods=['PUT'])
-@api.validate(tags=["Grille"], json=GrilleDimensions)
-def edit_grid(json: GrilleDimensions):
+
+@app.route('/grid/dimensions', methods=['PUT'])
+@api.validate(tags=["Grille"], json=GridSize)
+def edit_grid(json: GridSize):
     """
-    Modifie la grille avec les dimensions spécifiées
+    Modifie la grille avec les dimensions Hauteur-Largeur.
     """
     try:
         # Gestion taille trop grande/petite
@@ -55,7 +60,8 @@ def edit_grid(json: GrilleDimensions):
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/getGridWeights', methods=['GET'])
+
+@app.route('/grid/weights', methods=['GET'])
 @api.validate(tags=["Grille"])
 def get_grid_weights():
     """
@@ -70,6 +76,27 @@ def get_grid_weights():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
+@app.route('/grid/weights', methods=['PUT'])
+@api.validate(tags=["Grille"], json=GridWeights)
+def update_grid_weights(json: GridWeights):
+    """
+    Mise à jour des poids de plusieurs sommets spécifiques de la grille.
+    """
+    try:
+        columns = len(json.grid)
+        rows = len(json.grid[0])
+
+        if columns != grille.height or rows != grille.width:
+            return jsonify({"error": "La grille a une taille different de la requête"}), 400
+
+        for col in range(columns):
+            for row in range(rows):
+                grille.tab[col][row].set_weight(json.grid[col][row])
+
+        return jsonify({"message": "Tous les poids de la grille ont été mis à jour avec succès"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     api.register(app)
