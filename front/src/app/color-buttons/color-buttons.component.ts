6c6abb4766db924ca1d91bedda011e71ccaad774
courrierdsf <courrierdsf@gmail.com>
2025-01-10 16:47:04 +0100

    [Angular] style sur les bouton de couleurs

--- front/src/app/color-buttons/color-buttons.component.ts
+++ front/src/app/color-buttons/color-buttons.component.ts
@@ -1,16 +1,25 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-color-buttons',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './color-buttons.component.html',
   standalone: true,
   styleUrls: ['./color-buttons.component.css']
 })
 export class ColorButtonsComponent {
   @Output() colorChange = new EventEmitter<string>();
+  selectedColor: string = 'black'; // Noir pré-sélectionné par défaut
 
   changeColor(color: string): void {
+    this.selectedColor = color;
     this.colorChange.emit(color);
+  }
+
+  isColorUsed(color: string): boolean {
+    return this.selectedColor === color;
   }
 }
